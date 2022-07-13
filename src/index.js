b3cf119d960afb09ca46e28963d88bdc4d4513ea
Roman Sudakov <goa-slon@mail.ru>
2022-07-13 13:52:32 +0300

    init: start brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    
+    const stack = [];
+    const configObject = {};
+
+    bracketsConfig.forEach((item) => (configObject[item[0]] = item[1]));
+    
+    for (let i = 0; i < str.length; i++) {
+        if (configObject[stack[stack.length - 1]] === str[i]) {
+            stack.pop();
+        } else if (configObject.hasOwnProperty(str[i])) {
+            stack.push(str[i]);
+        } else {
+            return false;
+        }
+    }
+
+    return stack.length === 0;
+   
 }
+
